1bbe82c04ceb2fd9171e3d4e3b60b4f4df606df1
Misha <Mixail9122@gmail.com>
2022-11-19 23:50:22 +0300

    add manifest for k8s

--- src/main/java/com/example/dockerlearn/rest/UserController.java
+++ src/main/java/com/example/dockerlearn/rest/UserController.java
@@ -5,6 +5,7 @@ import com.example.dockerlearn.exeption.ResourceNotFoundException;
 import com.example.dockerlearn.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,21 +19,25 @@ public class UserController {
     private UserRepository userRepository;
 
     @PostMapping(path = "/create")
+    @Transactional
     public User creatUser(User user) {
         return userRepository.save(user);
     }
 
     @PostMapping(path = "/delete/{id}")
-    public void creatUser(@PathVariable Long id) {
+    @Transactional
+    public void deleteUser(@PathVariable Long id) {
         userRepository.deleteById(id);
     }
 
     @GetMapping
+    @Transactional
     public List<User> getUser() {
         return userRepository.findAll();
     }
 
     @GetMapping(path = "/{id}")
+    @Transactional
     public User getAllUser(@PathVariable Long id) {
         return userRepository.findById(id).orElseThrow(ResourceNotFoundException::new);
     }
